309527ba81a4a63e11b57dabbd63396fc8573041
trtrtr6 <2450260012@qq.com>
2018-09-26 10:29:12 +0800

    完善项目结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLodash from 'vue-lodash'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLodash)
 
 console.log(process.env.NODE_ENV)
 console.log(process.env.VUE_APP_TITLE)
