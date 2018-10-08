42c4b0e3b5d3962ac15e07c9de5a1ccb94e8c34b
trtrtr6 <2450260012@qq.com>
2018-10-08 15:25:21 +0800

    添加一些测试

---
+++ src/views/page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-button type="primary" class="el-icon-arrow-down"></el-button>
+    <el-date-picker v-model="time" placeholder=""></el-date-picker>
+    <el-dialog title="sdfsf" class=""></el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
